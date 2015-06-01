c682b0c6ebe4745a0e70967bd8f956d73bbd4806
laxika <laxika91@gmail.com>
2015-06-01 16:54:32 +0200

    improvements on reference name and id detection

--- src/main/java/com/isbncloud/iso639/api/Alpha4Database.java
+++ src/main/java/com/isbncloud/iso639/api/Alpha4Database.java
@@ -22,16 +22,40 @@ public class Alpha4Database {
         ArrayList<Alpha4> allAlpha4 = (ArrayList<Alpha4>) xstream.fromXML(xml);
 
         for (Alpha4 alpha4 : allAlpha4) {
-            referenceNameCache.put(alpha4.getReferenceName(), alpha4);
+            referenceNameCache.put(alpha4.getReferenceName().toLowerCase(), alpha4);
             alpha4IdCache.put(alpha4.getAlpha4Id(), alpha4);
         }
     }
 
-    public Alpha4 getAlpha4ByReferenceName(String alpha4ReferenceName) {
-        return referenceNameCache.get(alpha4ReferenceName);
+    public Alpha4 getAlpha4ByReferenceName(String referenceName) {
+        if (referenceName == null) {
+            return null;
+        }
+
+        return referenceNameCache.get(referenceName.toLowerCase());
+    }
+
+    public boolean isValidAlpha4ReferenceName(String referenceName) {
+        if (referenceName == null) {
+            return false;
+        }
+
+        return referenceNameCache.containsKey(referenceName.toLowerCase());
     }
 
     public Alpha4 getAlpha4ByAlpha4Id(String alpha4Id) {
-        return alpha4IdCache.get(alpha4Id);
+        if (alpha4Id == null) {
+            return null;
+        }
+
+        return alpha4IdCache.get(alpha4Id.toLowerCase());
+    }
+
+    public boolean isValidAlpha4Id(String alpha4Id) {
+        if (alpha4Id == null) {
+            return false;
+        }
+
+        return alpha4IdCache.containsKey(alpha4Id.toLowerCase());
     }
 }
